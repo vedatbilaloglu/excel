4a02746fa59fdf4d7b149d69b5f242f960f07e5f
Vedat <vedatbilaloglu@hotmail.com>
2021-03-23 10:06:16 +0300

    new commit
    new commit

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -20,7 +20,7 @@ namespace ConsoleApp2
 
 
 
-            string path = "C:\\Users\\HP\\Downloads\\Routerr.xlsx";
+            string path = "C:\\Users\\HP\\Downloads\\User.xlsx";
             FileInfo fileInfo = new FileInfo(path);
 
             ExcelPackage package = new ExcelPackage(fileInfo);
@@ -28,40 +28,46 @@ namespace ConsoleApp2
 
             // get number of rows and columns in the sheet
 
-            int rows = worksheet.Dimension.Rows; 
+            int rows = worksheet.Dimension.Rows;
             int columns = worksheet.Dimension.Columns;
 
             worksheet.Cells[2, 3].Value = "GizliDegil";
 
-            string[,] array = new string[rows+2, columns+1];
+            string[,] array = new string[rows + 2, columns + 1];
             var differentRowColumns = new List<int>();
 
-
-            string kontrol;
-            // loop through the worksheet rows and columns
+            // Excel tablosunu 2 boyutlu dizi içerisine yerlestirdim.
 
             for (int i = 2; i <= rows; i++)
             {
                 for (int j = 1; j <= columns; j++)
                 {
-                   
-                        object content = worksheet.Cells[i, j].Value;
-                        if(worksheet.Cells[i, j].Value == null)
-                        {
-                            array[i - 2, j - 1] = "";
-                            continue;
-                        }
-                        else
-                        {
-                            //Console.WriteLine("content: " + content);
-                            array[i - 2, j - 1] = worksheet.Cells[i, j].Value.ToString();
-                        }
-                                         
+                    object content = worksheet.Cells[i, j].Value;
+                    if (worksheet.Cells[i, j].Value == null)
+                    {
+                        array[i - 2, j - 1] = "";
+                        continue;
+                    }
+                    else
+                    {                      
+                        array[i - 2, j - 1] = worksheet.Cells[i, j].Value.ToString();
+                    }
                 }
             }
 
-            List<List<String>> ResultSet = new List<List<String>>();
+            for(int i = 0;i<rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine(array[i, j]);
+                }
+            }
 
+            //Console.WriteLine(ColumnHelper<string>("4", "0", array));
+            
+
+
+            List<string> differentID = new List<string>();
 
             // Create the Command and Parameter objects.
             SqlCommand command = new SqlCommand("SELECT * FROM routertable", connection);
@@ -70,12 +76,22 @@ namespace ConsoleApp2
 
             SqlDataReader reader = command.ExecuteReader();
             int k = 0,t=0;
+            int differentCounter = 0;
             while (reader.Read())
             {
                 var rec = new List<string>();
                 for (int i = 1; i < reader.FieldCount-1; i++) //The mathematical formula for reading the next fields must be <=
                 {
                     rec.Add(reader.GetString(i).ToString());
+                    if (i == 1)
+                    {
+                        if (!reader.GetString(i).Equals(array[k, 0]))
+                        {
+                            differentID.Add(array[k, 0]);
+                            differentCounter++; // id'ler farklı ise degeri 1 arttır.
+                            continue;
+                        }                       
+                    }
                     if (!reader.GetString(i).Equals(array[k, t]))
                     {
                         Console.WriteLine("Farkli olan deger: " + array[k,t]);
@@ -87,29 +103,62 @@ namespace ConsoleApp2
                 t = 0;
                 k++;
             }
-
             Console.WriteLine("***********");
-            foreach(var i in differentRowColumns)
+            Console.WriteLine("kaç tanesinin id karşılaştırması farklı geldi: " + differentCounter);
+            Console.WriteLine("***********");
+            
+            Console.WriteLine("Kaç farklı hücrede değişiklik yapıldı: " + (differentRowColumns.Count / 2));
+
+            //List<string> differentIdCount = differentRowColumns.Distinct().ToList().ToString();
+            List<string> differentIdCount = new List<string>();
+
+
+            // Hangi satırda değişik yapıldıysa o satırların id'sini verir.
+            for (int i = 0; i < differentRowColumns.Count; i += 2)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("different row and columns id: "
+                + ColumnHelper<string>(differentRowColumns[i].ToString(), "0", array));
+                differentIdCount.Add(ColumnHelper<string>(differentRowColumns[i].ToString(), "0", array));
             }
 
 
-        //for (int i = 0; i <= 499; i++)
-        //{
-        //    try
-        //    {
-        //        //var command1 = new SqlCommand("INSERT INTO routertable(tabloid,adi,gizliliksinifi,saklamayeri,butunluk,erisilebilirlik,gizlilik,varlikdegeri,konum,varlikmuhafizi,varliksahibi,ortam,sirket) Values('" + array[i, 0] + "','" + array[i, 1] + "','" + array[i, 2] + "','" + array[i, 3] + "','" + array[i, 4] + "','" + array[i, 5] + "','" + array[i, 6] + "','" + array[i, 7] + "','" + array[i, 8] + "','" + array[i, 9] + "','" + array[i, 10] + "','" + array[i, 11] + "','" + array[i, 12] + "')", connection);
-        //        //command1.ExecuteNonQuery();
+            // Aynı id üzerinde birden fazla hücrede işlem yapıldıysa, aynı id'yi 3-4 defa dönme 1 kere dön.
+            List<string> sayacArray = differentIdCount.Distinct().ToList();
+            Console.WriteLine("sayac array count: " + sayacArray.Count);
+
+            // Id'ler içinde router: şeklinde başlayan ifadeyi yok etmek için buradaki for ile düzenledim.
+            for(int j = 0; j<sayacArray.Count; j++)
+            {
+                sayacArray[j] = sayacArray[0].Substring(sayacArray[0].IndexOf(":") + 1);
+            }
+            Console.WriteLine("Yeni deger: " + sayacArray[0]);
 
 
-        //    }
-        //    catch (SqlException e)
-        //    {
-        //        throw;
-        //    }
-        //}
-    }
+            
+
+
+            //for (int i = 0; i <= 499; i++)
+            //{
+            //    try
+            //    {
+            //        //var command1 = new SqlCommand("INSERT INTO routertable(tabloid,adi,gizliliksinifi,saklamayeri,butunluk,erisilebilirlik,gizlilik,varlikdegeri,konum,varlikmuhafizi,varliksahibi,ortam,sirket) Values('" + array[i, 0] + "','" + array[i, 1] + "','" + array[i, 2] + "','" + array[i, 3] + "','" + array[i, 4] + "','" + array[i, 5] + "','" + array[i, 6] + "','" + array[i, 7] + "','" + array[i, 8] + "','" + array[i, 9] + "','" + array[i, 10] + "','" + array[i, 11] + "','" + array[i, 12] + "')", connection);
+            //        //command1.ExecuteNonQuery();
+
+
+            //    }
+            //    catch (SqlException e)
+            //    {
+            //        throw;
+            //    }
+            //}
+        }
+
+        public static T ColumnHelper<T>(T rowIndex, T columnName, T[,] array)
+        {
+            var rowIndexx = Convert.ToInt32(rowIndex.ToString());
+            var columnNamee = Convert.ToInt32(columnName.ToString());
+            return array[rowIndexx,columnNamee];
+        }
     }
 }
 
